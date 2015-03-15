e1181661e8cc8714bb60d8007f9829d7436e71da
sirihol <s.holtnaes@gmail.com>
2015-03-15 22:55:33 +0200

    navigator bar

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,25 @@
 $(document).ready(function(){  
 	$('[data-toggle=offcanvas]').click(function() {
 	    $('.row-offcanvas').toggleClass('active');
-  });
+	});
 
 	$(function() {
     	$('a.fullsizable').fullsizable();
-  });
+    });
+	
+	$('body').scrollspy({
+		target: '.bs-docs-sidebar', offset: 80
+	});
+	$('a[href^="#"]').on('click', function(event) {
+
+        var target = $( $(this).attr('href') );
+
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top -50
+            }, 600);
+        }
+    });
 
 });
